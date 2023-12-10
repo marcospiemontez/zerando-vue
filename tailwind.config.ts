d4122f70c353470a4179eb0f5fdc70f043d45d35
Marcos Piemontez <marcos.piemontez1@gmail.com>
2023-12-10 00:53:54 -0400

    fix(main) - first commit.

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.ts
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
